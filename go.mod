aa7ebd2e5d12142c0397ec3b765fce37cc3f203b
Abhishek Sah <abhi.sah.97@gmail.com>
2024-11-13 11:40:01 +0530

    add all

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang_import_poc
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"golang_import_poc/calculator"
+)
+
+func main() {
+	fmt.Println(calculator.Add(1, 2))
+	return
+}
