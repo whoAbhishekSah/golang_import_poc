2388714d70d8098b5753c15fa4b283c5dc351b88
Abhishek Sah <abhi.sah.97@gmail.com>
2024-11-13 11:51:47 +0530

    add internal calculator

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/whoAbhishekSah/golang_import_poc/calculator"
+	"github.com/whoAbhishekSah/golang_import_poc/internal"
 )
 
 func main() {
 	fmt.Println(calculator.Add(1, 2))
+	fmt.Println(internal.Add(1, 2))
 	return
 }
